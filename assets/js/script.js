7161e36704244f50d78878d088f56a8544aa1a79
constantijn van hartesveldt <cvanh>
2020-10-11 14:49:15 +0200

    gg

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,85 @@
-document.getElementById("c").onclick = function () { // als op de c-toets wordt geklikt
-	var c_toets = document.getElementById("audio_c"); // zoek het c-toets audio-element op
-	c_toets.play(); // speel het audio-element af
+// octaaf 1
+
+document.getElementById("c1").onclick = function () {
+	var c1_toets = document.getElementById("audio_c1");
+	c1_toets.play();
 }
-document.getElementById("d").onclick = function () {
-	var d_toets = document.getElementById("audio_d");
+document.getElementById("cc1").onclick = function () {
+	var cc1_toets = document.getElementById("audio_cc1");
 	d_toets.play();
 }
-document.getElementById("e").onclick = function () {
-	var e_toets = document.getElementById("audio_e");
+document.getElementById("d1").onclick = function () {
+	var d_toets = document.getElementById("audio_d1");
+	d_toets.play();
+}
+document.getElementById("dd1").onclick = function () {
+	var d_toets = document.getElementById("audio_dd1");
+	d_toets.play();
+}
+document.getElementById("e1").onclick = function () {
+	var d_toets = document.getElementById("audio_e1");
+	d_toets.play();
+}
+document.getElementById("ee1").onclick = function () {
+	var d_toets = document.getElementById("audio_ee1");
+	d_toets.play();
+}
+document.getElementById("f1").onclick = function () {
+	var d_toets = document.getElementById("audio_f1");
+	d_toets.play();
+}
+document.getElementById("ff1").onclick = function () {
+	var d_toets = document.getElementById("audio_ff1");
+	d_toets.play();
+}
+document.getElementById("g1").onclick = function () {
+	var d_toets = document.getElementById("audio_g1");
+	d_toets.play();
+}
+document.getElementById("gg1").onclick = function () {
+	var d_toets = document.getElementById("audio_gg1");
+	d_toets.play();
+}
+document.getElementById("a1").onclick = function () {
+	var d_toets = document.getElementById("audio_a1");
+	d_toets.play();
+}
+document.getElementById("b1").onclick = function () {
+	var d_toets = document.getElementById("audio_b1");
+	d_toets.play();
+}
+
+
+
+
+// octaaf 2 
+
+document.getElementById("c2").onclick = function () { // als op de c-toets wordt geklikt
+	var c_toets = document.getElementById("audio_c2"); // zoek het c-toets audio-element op
+	c_toets.play(); // speel het audio-element af
+}
+document.getElementById("d2").onclick = function () {
+	var d_toets = document.getElementById("audio_d2");
+	d_toets.play();
+}
+document.getElementById("e2").onclick = function () {
+	var e_toets = document.getElementById("audio_e2");
 	e_toets.play();
 }
-document.getElementById("f").onclick = function () {
-	var f_toets = document.getElementById("audio_f");
+document.getElementById("f2").onclick = function () {
+	var f_toets = document.getElementById("audio_f2");
 	f_toets.play();
 }
-document.getElementById("g").onclick = function () {
-	var g_toets = document.getElementById("audio_g");
+document.getElementById("g2").onclick = function () {
+	var g_toets = document.getElementById("audio_g2");
 	g_toets.play();
 }
-document.getElementById("a").onclick = function () {//deze doen het niet
-	var a_toets = document.getElementById("audio_a");
+document.getElementById("a2").onclick = function () {//deze doen het niet
+	var a_toets = document.getElementById("audio_a2");
 	a_toets.play();
 }
-document.getElementById("b").onclick = function () {// deze doen het niet
-	var b_toets = document.getElementById("audio_b");
+document.getElementById("b2").onclick = function () {// deze doen het niet
+	var b_toets = document.getElementById("audio_b2");
 	b_toets.play();
 }
 //<!C - C# - D - D# - E - F - F# - G - G# - A - A# - B>
